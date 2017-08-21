794574a5c7f303a06b28b0cd3423b90d6fd7bc63
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2017-08-21 13:44:29 +0200

    Support null value for more types

--- app/helpers/db/query/Query.java
+++ app/helpers/db/query/Query.java
@@ -6,6 +6,9 @@ import org.hibernate.internal.SQLQueryImpl;
 import org.hibernate.type.StandardBasicTypes;
 import play.db.jpa.JPA;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  * Query builder that encapsulates JPA native query builder, and handles null values properly.
  *
@@ -23,8 +26,43 @@ public class Query {
         return this;
     }
 
+    public Query setParameter(String key, Short value) {
+        getInternalQuery().setParameter(key, value, StandardBasicTypes.SHORT);
+        return this;
+    }
+
+    public Query setParameter(String key, Integer value) {
+        getInternalQuery().setParameter(key, value, StandardBasicTypes.INTEGER);
+        return this;
+    }
+
+    public Query setParameter(String key, Long value) {
+        getInternalQuery().setParameter(key, value, StandardBasicTypes.BIG_INTEGER);
+        return this;
+    }
+
+    public Query setParameter(String key, Float value) {
+        getInternalQuery().setParameter(key, value, StandardBasicTypes.FLOAT);
+        return this;
+    }
+
     public Query setParameter(String key, Double value) {
         getInternalQuery().setParameter(key, value, StandardBasicTypes.DOUBLE);
+        return this;
+    }
+
+    public Query setParameter(String key, BigDecimal value) {
+        getInternalQuery().setParameter(key, value, StandardBasicTypes.BIG_DECIMAL);
+        return this;
+    }
+
+    public Query setParameter(String key, Boolean value) {
+        getInternalQuery().setParameter(key, value, StandardBasicTypes.BOOLEAN);
+        return this;
+    }
+
+    public Query setParameter(String key, Date value) {
+        getInternalQuery().setParameter(key, value, StandardBasicTypes.TIMESTAMP);
         return this;
     }
 
